a000ce94eb199139b099494bf0a30571391b57a7
konenikushal <konenikushal@gmail.com>
2023-12-04 13:46:49 -0500

    1414

--- itis3135/Activity14/Koneni-Activity14.js
+++ itis3135/Activity14/Koneni-Activity14.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
-	
-}); // end ready
+    $('#nav_list a').click(function(e) {
+        e.preventDefault();
+
+        var title = $(this).attr('title');
+        var jsonFile = 'json_files/' + title + '.json';
+
+        $.getJSON(jsonFile, function(data) {
+            var htmlContent = '';
+
+            $.each(data.speakers, function(index, speaker) {
+                htmlContent += '<h1>' + speaker.title + '</h1>';
+                htmlContent += '<h2>' + speaker.month + '</h2>';
+                htmlContent += '<h3>' + speaker.speaker + '</h3>';
+                htmlContent += '<img src="' + speaker.image + '" alt="' + speaker.speaker + '_picture">';
+                htmlContent += '<p>' + speaker.text + '</p>';
+            });
+
+            $('main').empty();
+            $('main').html(htmlContent);
+        });
+    });
+});
